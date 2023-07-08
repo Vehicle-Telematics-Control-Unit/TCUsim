9fd8e8f38f90f08f22ba2c14451d97e37eb80f4f
Princeofguilty <Princeofguilty@hotmail.com>
2023-07-09 02:43:53 +0300

    Algorithm 2-cases deployed

--- Assets/CollisionPrediction.cs
+++ Assets/CollisionPrediction.cs
@@ -31,95 +31,75 @@ public class CollisionPrediction : MonoBehaviour
         collide = false;
         foreach (Vehicle v in surrounding_vehicles)
         {
-            if ((my_vehicle.angle == 90 || my_vehicle.angle == 270) && (v.angle == 90 || v.angle == 270))
+            if (Math.Abs(my_vehicle.x - v.x) <= RADIUS * 2 && Math.Abs(my_vehicle.z - v.z) <= RADIUS * 2)
             {
-                if (Math.Abs(my_vehicle.z - v.z) > RADIUS * 2
-                || ((my_vehicle.angle == 270 && v.angle == 90 && v.x > my_vehicle.x)
-                || (my_vehicle.angle == 90 && v.angle == 270 && v.x < my_vehicle.x)
-                || (my_vehicle.speed == 0 && v.speed == 0)
-                || (my_vehicle.angle == v.angle && my_vehicle.speed == v.speed)
-                || (my_vehicle.angle == 90 && v.angle == 90 && my_vehicle.speed > v.speed && my_vehicle.x > v.x)
-                || (my_vehicle.angle == 90 && v.angle == 90 && my_vehicle.speed < v.speed && my_vehicle.x < v.x)
-                || (my_vehicle.angle == 270 && v.angle == 270 && my_vehicle.speed > v.speed && my_vehicle.x < v.x)
-                || (my_vehicle.angle == 270 && v.angle == 270 && my_vehicle.speed < v.speed && my_vehicle.x > v.x)
-                && Math.Abs(my_vehicle.x - v.x) > RADIUS * 2))
-                {
-                    Debug.Log("Collision :: no collision");
-                    continue;
+                time2Collide = 0;
+                Debug.Log("Collision :: touching each other");
+                break;
+            }
+            else if (Math.Abs(v.angle - my_vehicle.angle) == 180 || Math.Abs(v.angle - my_vehicle.angle) == 0)
+            {
+                Debug.Log("Collision :: Lines are parallel");
 
+                if ((my_vehicle.angle == 90 || my_vehicle.angle == 270) && (v.angle == 90 || v.angle == 270))
+                {
+                    Debug.Log("Collision :: Angle = 90 or 270");
+                    if (Math.Abs(my_vehicle.z - v.z) > RADIUS * 2
+                        || ((my_vehicle.angle == 270 && v.angle == 90 && v.x > my_vehicle.x)
+                        || (my_vehicle.angle == 90 && v.angle == 270 && v.x < my_vehicle.x)
+                        || (my_vehicle.speed == 0 && v.speed == 0)
+                        || (my_vehicle.angle == v.angle && my_vehicle.speed == v.speed)
+                        || (my_vehicle.angle == v.angle && my_vehicle.speed > v.speed && my_vehicle.x > v.x)
+                        || (my_vehicle.angle == v.angle && my_vehicle.speed < v.speed && my_vehicle.x < v.x)
+                        && Math.Abs(my_vehicle.x - v.x) > RADIUS * 2))
+                    {
+                        Debug.Log("Collision :: Angle = 90 or 270 but no collision");
+                        continue;
+                    }
                 }
                 else
                 {
-                    if (Math.Abs(my_vehicle.x - v.x) <= RADIUS * 2)
+                    Debug.Log("Collision :: Angle != 90 nor 270");
+                    float m = (float)Math.Tan(Mathf.Deg2Rad * my_vehicle.angle);
+                    float c = my_vehicle.x - m * my_vehicle.z;
+                    float d = (float)Math.Abs((m * v.z - v.x + c) / Math.Sqrt(m * m + 1));
+                    // Debug.Log("Collision :: D="+d.ToString());
+                    if (d > RADIUS * 2
+                        || (((my_vehicle.angle > 180 && my_vehicle.angle < 270 && v.angle >= 0 && v.angle < 90) || (my_vehicle.angle > 270 && my_vehicle.angle < 360 && v.angle > 90 && v.angle <= 180)) && v.z > my_vehicle.z)
+                        || (((my_vehicle.angle >= 0 && my_vehicle.angle < 90 && v.angle > 180 && v.angle < 270) || (my_vehicle.angle > 90 && my_vehicle.angle <= 180 && v.angle > 270 && v.angle < 360)) && v.z < my_vehicle.z)
+                        || (my_vehicle.speed == 0 && v.speed == 0)
+                        || ((my_vehicle.speed == v.speed
+                        || ((my_vehicle.angle >= 0 && my_vehicle.angle < 90) || (my_vehicle.angle > 270 && my_vehicle.angle < 360) && my_vehicle.speed > v.speed && my_vehicle.z > v.z)
+                        || ((my_vehicle.angle >= 0 && my_vehicle.angle < 90) || (my_vehicle.angle > 270 && my_vehicle.angle < 360) && my_vehicle.speed < v.speed && my_vehicle.z < v.z)
+                        || ((my_vehicle.angle > 180 && my_vehicle.angle < 270) || (my_vehicle.angle > 90 && my_vehicle.angle <= 180) && my_vehicle.speed > v.speed && my_vehicle.z < v.z)
+                        || ((my_vehicle.angle > 180 && my_vehicle.angle < 270) || (my_vehicle.angle > 90 && my_vehicle.angle <= 180) && my_vehicle.speed < v.speed && my_vehicle.z > v.z))) && my_vehicle.angle == v.angle)
                     {
-                        Debug.Log("Collision :: touching each other");
-                        time2Collide = 0;
-                        break;
+                        Debug.Log("Collision :: Angle != 90 nor 270 and no collision");
+                        continue;
                     }
-                    else
-                    {
-                        Debug.Log("Collision :: Calculating time");
-                        if (my_vehicle.angle == v.angle)
-                        {
-                            time2Collide = Math.Abs(my_vehicle.x - v.x - RADIUS * 2) / Math.Abs(my_vehicle.speed - v.speed);
-                            Debug.Log("Collision :: Calculating time, Angles are equal");
-
-                        }
-                        else
-                        {
-                            time2Collide = Math.Abs(my_vehicle.x - v.x - RADIUS * 2) / (my_vehicle.speed + v.speed);
-                            Debug.Log("Collision :: Calculating time, Angles aren't equal");
-
-                        }
-                    }
+                }
+                if (my_vehicle.angle == v.angle)
+                {
+                    time2Collide = (float)(Math.Abs(Math.Sqrt(Math.Pow(my_vehicle.x - v.x, 2) + Math.Pow(my_vehicle.z - v.z, 2)) - RADIUS * 2) / Math.Abs(my_vehicle.speed - v.speed));
+                    Debug.Log("Collision :: Calculating AngleV = AngleM");
+                }
+                else
+                {
+                    time2Collide = (float)(Math.Abs(Math.Sqrt(Math.Pow(my_vehicle.x - v.x, 2) + Math.Pow(my_vehicle.z - v.z, 2)) - RADIUS * 2) / (my_vehicle.speed + v.speed));
+                    Debug.Log("Collision :: Calculating AngleV != AngleM");
                 }
             }
 
 
-            // else
-            // {
-            //     if (Math.Tan(my_vehicle.angle) == Math.Tan(v.angle))
-            //     {
-            //         float m = (float)Math.Tan(my_vehicle.angle);
-            //         float c = my_vehicle.x - m * my_vehicle.z;
-            //         float d = (float)Math.Abs((m * v.z - v.x + c) / Math.Sqrt(m * m + 1));
-            //         if (d > RADIUS * 2
-            //         || ((((my_vehicle.angle > 180 && my_vehicle.angle < 270 && v.angle >= 0 && v.angle < 90) || (my_vehicle.angle > 270 && my_vehicle.angle < 360 && v.angle > 90 && v.angle <= 180)) && v.z > my_vehicle.z)
-            //         || (((my_vehicle.angle >= 0 && my_vehicle.angle < 90 && v.angle > 180 && v.angle < 270) || (my_vehicle.angle > 90 && my_vehicle.angle <= 180 && v.angle > 270 && v.angle < 360)) && v.z < my_vehicle.z)
-            //         || (my_vehicle.speed == 0 && v.speed == 0)
-            //         || (my_vehicle.speed == v.speed
-            //         || ((my_vehicle.angle >= 0 && my_vehicle.angle < 90) || (my_vehicle.angle > 270 && my_vehicle.angle < 360) && my_vehicle.speed > v.speed && my_vehicle.z > v.z)
-            //         || ((my_vehicle.angle >= 0 && my_vehicle.angle < 90) || (my_vehicle.angle > 270 && my_vehicle.angle < 360) && my_vehicle.speed < v.speed && my_vehicle.z < v.z)
-            //         || ((my_vehicle.angle > 180 && my_vehicle.angle < 270) || (my_vehicle.angle > 90 && my_vehicle.angle <= 180) && my_vehicle.speed > v.speed && my_vehicle.z < v.z)
-            //         || ((my_vehicle.angle > 180 && my_vehicle.angle < 270) || (my_vehicle.angle > 90 && my_vehicle.angle <= 180) && my_vehicle.speed < v.speed && my_vehicle.z > v.z) && my_vehicle.angle == v.angle)
-            //         && Math.Abs(my_vehicle.x - v.x) > RADIUS * 2 && Math.Abs(my_vehicle.z - v.z) > RADIUS * 2))
-            //             continue;
-            //         else
-            //         {
-            //             if (Math.Abs(my_vehicle.x - v.x) <= RADIUS * 2 && Math.Abs(my_vehicle.z - v.z) <= RADIUS * 2)
-            //             {
-            //                 time2Collide = 0;
-            //                 break;
-            //             }
-            //             else
-            //             {
-            //                 if (my_vehicle.angle == v.angle)
-            //                     time2Collide = (float)Math.Abs(Math.Sqrt(Math.Pow(my_vehicle.x - v.x, 2) + Math.Pow(my_vehicle.z - v.z, 2)) - RADIUS * 2) / Math.Abs(my_vehicle.speed - v.speed);
-            //                 else
-            //                     time2Collide = (float)Math.Abs(Math.Sqrt(Math.Pow(my_vehicle.x - v.x, 2) + Math.Pow(my_vehicle.z - v.z, 2)) - RADIUS * 2) / (my_vehicle.speed + v.speed);
-            //             }
-            //         }
-            //     }
-            // }
 
-            
             if (time2Collide <= time_threshold)
             {
                 Debug.Log("Collision @ " + time2Collide.ToString());
-                Capsule.material.color = new Color(1,0,0);
+                Capsule.material.color = new Color(1, 0, 0);
             }
-            else{
-                Capsule.material.color = new Color(0,0,0);
+            else
+            {
+                Capsule.material.color = new Color(1, 1, 1);
             }
         }
 
--- Assets/my_active_vehicle.cs
+++ Assets/my_active_vehicle.cs
@@ -5,6 +5,7 @@ using System.Net.Sockets;
 using System.Text;
 using System;
 using System.IO;
+using UnityEngine.SceneManagement;
 
 public class my_active_vehicle : MonoBehaviour
 {
@@ -16,6 +17,7 @@ public class my_active_vehicle : MonoBehaviour
     public Vector3 velocity;
 
     public Rigidbody RB;
+    MeshRenderer Capsule;
 
     void Start()
     {
@@ -28,6 +30,8 @@ public class my_active_vehicle : MonoBehaviour
         Vector3 newRotation = transform.eulerAngles;
         newRotation.y = float.Parse(parameters[2]); // Set the desired rotation value
         transform.eulerAngles = newRotation;
+        reader.Close();
+        Capsule = GameObject.Find("Capsule").GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -40,13 +44,27 @@ public class my_active_vehicle : MonoBehaviour
         Base tower = Base.tower;
         x = this.transform.position.x / tower.scale + tower.base_station_lat;
         z = this.transform.position.z / tower.scale + tower.base_station_lon;
-        angle = transform.rotation.eulerAngles.y; 
+        angle = transform.rotation.eulerAngles.y;
         speed = RB.velocity.magnitude;
         velocity = RB.velocity;
 
         z = Mathf.Round(z * 100000) / 100000;
         x = Mathf.Round(x * 100000) / 100000;
         angle = Mathf.Round(angle * 10) / 10;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StreamReader reader = new StreamReader("unity.txt");
+            string line = reader.ReadLine(); // x, z, angle
+            string[] parameters = line.Split(',');
+            this.transform.position = new Vector3(float.Parse(parameters[0]), 0, float.Parse(parameters[1]));
+
+            Vector3 newRotation = transform.eulerAngles;
+            newRotation.y = float.Parse(parameters[2]); // Set the desired rotation value
+            transform.eulerAngles = newRotation;
+            reader.Close();
+            Capsule.material.color = new Color(1, 1, 1);
+        }
     }
 
     void broadcast()
@@ -56,7 +74,7 @@ public class my_active_vehicle : MonoBehaviour
         if (socket != null)
         {
             // sscanf(buffer, "l:%f,%f&&h:%f&&s:%d&&b:%d", lat, lon, heading, speed, brakes);
-            string message = "l:"+ x.ToString() + "," + z.ToString() + "&&h:" + (Convert.ToInt16(angle)).ToString() + "&&s:"+ Convert.ToInt32(speed).ToString() +"&&b:0"; // lat.ToString() + "," + lon.ToString() + "," + angle.ToString();
+            string message = "l:" + x.ToString() + "," + z.ToString() + "&&h:" + (Convert.ToInt16(angle)).ToString() + "&&s:" + Convert.ToInt32(speed).ToString() + "&&b:0"; // lat.ToString() + "," + lon.ToString() + "," + angle.ToString();
             Debug.Log(message);
             byte[] data = Encoding.ASCII.GetBytes(message);
             socket.Send(data);
